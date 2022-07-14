c87d3aa48428a0163c78966f7efdda948329c94d
jun-uen0 <hello@junueno.dev>
2022-07-14 22:10:16 +0900

    fuck me type maker

--- server/auth_utils.py
+++ server/auth_utils.py
@@ -12,18 +12,18 @@ class AuthJwtCsrf():
   pwd_ctx = CryptContext(schemes=['bcrypt'], deprecated='auto')
   secret_key = JWT_KEY
 
-  def generate_hashed_pw(selt, pw):
-    return selt.pwd_ctx.hash(pw)
-  
-  def verify_pw(self,plain_pw, hashed_pw):
+  def generate_hashed_pw(self, password) -> str:
+    return self.pwd_ctx.hash(password)
+
+  def verify_pw(self,plain_pw, hashed_pw) -> bool:
     return self.pwd_ctx.verify(plain_pw, hashed_pw)
-  
-  def encode_jwt(self,email):
+
+  def encode_jwt(self,email) -> str:
     payload = {
       # datetime.utcnow(): returns the current UTC date and time, as a naive datetime object.
       # timedelta(): returns a timedelta object representing a duration of time.
       'exp': datetime.utcnow() + timedelta(days=0, minutes=5), # JWT Token will expride in 5 minutes
-      'iat': '',
+      'iat': datetime.utcnow(), # JWT Token will be created at the time of issuing
       'sub': email
     }
     # Generate JWT Token
@@ -33,16 +33,16 @@ class AuthJwtCsrf():
       algorithm='HS256' # HS256 requires one secret key
     )
 
-  def decode_jwt(self,token):
+  def decode_jwt(self,token) -> str:
     try:
       payload = jwt.decode(token, self.secret_key, algorithms=['HS256'])
       return payload['sub']
     except jwt.ExpiredSignatureError:
       raise HTTPException(status_code=401, detail='Token expired')
-    except jwt.InvalidTokenError:
+    except jwt.InvalidTokenError as e: # Added 'as e' to catch the exception
       raise HTTPException(status_code=401, detail='Invalid token')
     
-  def verify_jwt(self,request):
+  def verify_jwt(self,request) -> str:
     # Get access token from Cookie
     token = request.cookies.get('access_token')
     if not token:
@@ -53,13 +53,13 @@ class AuthJwtCsrf():
     return subject
 
   def verify_update_jwt(self, request) -> tuple[str,str]:
-    subject = self.verity_jwt(request) # Get the email from the JWT token
+    subject = self.verify_jwt(request) # Get the email from the JWT token
     new_token = self.encode_jwt(subject) # Generate a new JWT token
     return new_token, subject # Return the new JWT token and the email from the JWT token
 
-  def verify_update_jwt_csrf(self, request, csrf_protect, headers) -> tuple[str,str]:
+  def verify_csrf_update_jwt(self, request, csrf_protect, headers) -> str:
     csrf_token = csrf_protect.get_csrf_from_headers(headers) # Get the CSRF token from the headers
-    csrf_protect = csrf_protect.validate_csrf(csrf_token) # Validate the CSRF token
-    subject = self.verity_jwt(request) # Get the email from the JWT token
+    csrf_protect.validate_csrf(csrf_token) # Validate the CSRF token
+    subject = self.verify_jwt(request) # Get the email from the JWT token
     new_token = self.encode_jwt(subject) # Generate a new JWT token
     return new_token # Return the new JWT token
